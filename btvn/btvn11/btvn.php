ef89494cbd2dda5f9a0c85ebfa73103e4676f6b0
danng <dannt.c25@gmail.com>
2018-08-23 23:54:33 +0700

    thg

---
+++ btvn/btvn11/btvn.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: danng
+ * Date: 23/08/2018
+ * Time: 23:38
+ */
+    $php = "php";
+    $zen = "zen framework";
+    $joomla = "joomla";
+
+    $courses = array();
+    $courses[] = "php";
+    $courses[] = "zen framework";
+    $courses[] = "joomla";
+
+    $length =count($courses);
+    echo $length;
